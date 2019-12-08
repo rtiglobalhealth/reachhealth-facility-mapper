0f4f4eb25570d622659cbfe64ef7738269f71b32
Adam Preston <adam.m.preston@gmail.com>
2019-12-08 09:40:15 +0800

    added sqlite

---
+++ README.md
@@ -0,0 +1,11 @@
+# Overview
+This tool will look up the facility UIDs using the facility name. This can be used when creating the data import files for DHIS2.
+
+### Getting started
+ To start the server
+
+ 1. install python
+ 2. run pip install -r requirements.txt
+ 3. python facility_ids.py
+ 4. Go to the server and upload the csv file. See example.csv for an example
+ 5. Paste output in import file.
--- facility_ids.py
+++ facility_ids.py
@@ -1,11 +1,19 @@
 import os
 import urllib.request
-
+from flask_sqlalchemy import SQLAlchemy
 from flask import Flask, flash, request, redirect, render_template
 from werkzeug.utils import secure_filename
 
 app = Flask(__name__)
 ALLOWED_EXTENSIONS = set(['csv'])
+basedir = os.path.abspath(os.path.dirname(__file__))
+
+class Config(object):
+    # ...
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
+        'sqlite:///' + os.path.join(basedir, 'orgs.sqlite')
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+
 
 def allowed_file(filename):
 	return '.' in filename and filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
@@ -16,24 +24,33 @@ def upload_form():
 
 @app.route('/', methods=['POST'])
 def upload_file():
-	if request.method == 'POST':
+    if request.method == 'POST':
         # check if the post request has the file part
-		if 'file' not in request.files:
-			flash('No file part')
-			return redirect(request.url)
-		file = request.files['file']
-		if file.filename == '':
-			flash('No file selected for uploading')
-			return redirect(request.url)
-		if file and allowed_file(file.filename):
-			filename = secure_filename(file.filename)
+        if 'file' not in request.files:
+            flash('No file part')
+            return redirect(request.url)
+
+        file = request.files['file']
+        
+        if file.filename == '':
+            flash('No file selected for uploading')
+            return redirect(request.url)
+        if file and allowed_file(file.filename):
+            filename = secure_filename(file.filename)
+            uids=[]
+            uids.append("hello")
+            uids.append("there")
+            adam = "hello htere"
+            #flash('File successfully uploaded')
 			#file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
-			flash('File successfully uploaded')
-			return redirect('/')
-		else:
-			flash('Allowed file types are txt, pdf, png, jpg, jpeg, gif')
-			return redirect(request.url)
+            return render_template("results.html", message=adam, uids = uids);
+        else:
+            flash('The only allowed file types is csv')
+            return redirect(request.url)
+        
 
 if __name__ == "__main__":
     app.secret_key = os.urandom(24)
-    app.run()
+    app.config.from_object(Config)
+    db = SQLAlchemy(app)
+    app.run(host='0.0.0.0')
